aaa45450a8cdf123174a5f035979fecc199b0bfb
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-10 01:43:19 +0800

    init Spring Boot AI community project

---
+++ src/main/java/com/ai/aicommunity1/AiCommunity1Application.java
@@ -0,0 +1,13 @@
+package com.ai.aicommunity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCommunity1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCommunity1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/aicommunity1/AiCommunity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.aicommunity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommunity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
