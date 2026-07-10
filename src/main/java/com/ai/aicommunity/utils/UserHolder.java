f7ebb1a804ab53a9c51ee40e3168966f859580e2
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-11 01:18:48 +0800

    add UserHolder for current login user

---
+++ src/main/java/com/ai/aicommunity/utils/UserHolder.java
@@ -0,0 +1,18 @@
+package com.ai.aicommunity.utils;
+
+public class UserHolder {
+
+    private static final ThreadLocal<Long> USER_ID = new ThreadLocal<>();
+
+    public static void saveUserId(Long userId) {
+        USER_ID.set(userId);
+    }
+
+    public static Long getUserId() {
+        return USER_ID.get();
+    }
+
+    public static void remove() {
+        USER_ID.remove();
+    }
+}
