5338d05bedd6ea951c085c37d8134a2bc2231836
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-10 19:59:59 +0800

    add mybatis plus configuration

---
+++ src/main/java/com/ai/aicommunity/config/MyBatisPlusConfig.java
@@ -0,0 +1,10 @@
+package com.ai.aicommunity.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.ai.aicommunity.mapper")
+public class MyBatisPlusConfig {
+
+}
