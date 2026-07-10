d03f050b9759ba0b50da2170849eda7a0f2c9871
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-11 03:32:59 +0800

    add article mapper

---
+++ src/main/java/com/ai/aicommunity/mapper/ArticleMapper.java
@@ -0,0 +1,9 @@
+package com.ai.aicommunity.mapper;
+
+import com.ai.aicommunity.entity.Article;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleMapper extends BaseMapper<Article> {
+}
