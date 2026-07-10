3b99c566fcb35c38e9be6afc1a56c092f3a0331f
uly609 <78928321+uly609@users.noreply.github.com>
2026-07-10 22:15:19 +0800

    add user controller layer

---
+++ src/main/java/com/ai/aicommunity/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.ai.aicommunity.controller;
+
+import com.ai.aicommunity.entity.User;
+import com.ai.aicommunity.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> list() {
+        return userService.list();
+    }
+}
